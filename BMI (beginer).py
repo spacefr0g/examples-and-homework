a907e49b732554b2ca99fcda4a2ba80df811209e
spacefr0g <42841447+spacefr0g@users.noreply.github.com>
2018-09-13 10:46:14 +0300

    bmi calculation

---
+++ BMI (beginer).py
@@ -0,0 +1,12 @@
+
+
+
+
+
+def bmi (x, a,):
+    return (x/(a**2))
+name = str(input("enter you name "))
+x = int(input ("enter you weight "))
+a = float(input ("inter you height in meters "))
+
+print (name, "you BMI is ", bmi (x,a))
